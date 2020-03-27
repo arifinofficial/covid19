2653f99a0da0e82f6f173ba27a44cbc099e6f510
Arifin N <arifinofficial30@gmail.com>
2020-03-27 09:03:43 +0800

    About template

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+
+<v-container fluid>
+  <div class="about text-center">
+    <h1 class="display-3 pb-4 pt-4">Halo...</h1>
+    <v-spacer></v-spacer>
+    <h2 class="headline">Hubungi <a href="mailto:hello@finlabs.dev">hello@finlabs.dev</a> untuk report bug atau saran.</h2>
+    <v-card class="text-left mt-10">
+      <v-card-title>
+        Data Source
+      </v-card-title>
+      <v-card-subtitle>
+        Data repository operated by the Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE) <a target="_blank" href="https://github.com/CSSEGISandData/COVID-19">https://github.com/CSSEGISandData/COVID-19</a>
+        <br>
+        ExpDev07 <a target="_blank" href="https://github.com/ExpDev07/coronavirus-tracker-api">https://github.com/ExpDev07/coronavirus-tracker-api</a>
+        <br>
+        KawalCorona <a target="_blank" href="https://kawalcorona.com/">https://kawalcorona.com/</a>
+        <br>
+        Astsiatsko <a target="_blank" href="https://rapidapi.com/astsiatsko/api/coronavirus-monitor">https://rapidapi.com/astsiatsko/api/coronavirus-monitor</a>
+        <br>
+        World Health Organization (WHO) <a target="_blank" href="https://www.who.int/">https://www.who.int/</a>
+      </v-card-subtitle>
+    </v-card>
   </div>
+</v-container>
+
 </template>
